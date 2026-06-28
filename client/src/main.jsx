14dd79bf2eeff0b93b4fbe472ad716729ea0e08f
Ashutosh Kumar <ak4173js@gmail.com>
2026-06-28 13:58:59 +0530

    Wrap application with theme provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,26 +4,35 @@ import "./index.css";
 import App from "./App";
 
 import { TaskProvider } from "./context/TaskContext";
+import { ThemeProvider } from "./context/ThemeContext";
+
 import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <TaskProvider>
-      <App />
 
-      {/* Toast Notifications */}
-      <Toaster
-        position="top-right"
-        reverseOrder={false}
-        toastOptions={{
-          duration: 3000,
-          style: {
-            borderRadius: "12px",
-            background: "#0f172a",
-            color: "#fff",
-          },
-        }}
-      />
-    </TaskProvider>
+    <ThemeProvider>
+
+      <TaskProvider>
+
+        <App />
+
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 3000,
+            style: {
+              borderRadius: "12px",
+              background: "#0f172a",
+              color: "#fff",
+            },
+          }}
+        />
+
+      </TaskProvider>
+
+    </ThemeProvider>
+
   </React.StrictMode>
 );
